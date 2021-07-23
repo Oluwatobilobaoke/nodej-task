5a75dd0d3a8f2285a31b726746993ae27f79edc7
Pheranmie <tobi.okekunle@reventtechnologies.com>
2021-07-23 13:40:54 +0100

    initial commit, code setup and home api route

---
+++ error-handler.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, req, res, next) => {
+  if (typeof (err) === 'string') {
+    // custom application error
+    return res.status(400).json({ status: 'error', message: err });
+  }
+
+  // default to 500 server error
+  return res.status(500).json({ status: 'error', message: err.message });
+};
+
+module.exports.errorHandler = errorHandler;
